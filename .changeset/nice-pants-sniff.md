6005fd5c4dbfa601de88209c362136b31a611b9b
roboin <50550561+Robot-Inventor@users.noreply.github.com>
2024-05-01 23:48:20 +0900

    fix: change import assertion to import attribute (#30)

---
+++ .changeset/nice-pants-sniff.md
@@ -0,0 +1,5 @@
+---
+"@robot-inventor/shell-session-syntax": patch
+---
+
+fix: change import assertion to import attribute
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import grammar from "../syntaxes/shell-session.tmLanguage.json" assert { type: "json" };
+import grammar from "../syntaxes/shell-session.tmLanguage.json" with { type: "json" };
 import type { LanguageRegistration } from "shiki";
 
 export default grammar as unknown as LanguageRegistration;
